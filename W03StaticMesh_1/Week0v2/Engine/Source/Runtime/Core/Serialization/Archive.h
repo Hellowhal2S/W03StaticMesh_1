7636f3d5d172c52515247a40d8c9c22eb041fb00
gudtldn <gudtldn1234@gmail.com>
2025-03-26 14:55:32 +0900

    wip

---
+++ W03StaticMesh_1/Week0v2/Engine/Source/Runtime/Core/Serialization/Archive.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "HAL/PlatformType.h"
+
+
+class FArchive {
+public:
+    virtual ~FArchive() = default;
+    
+    virtual void Serialize(void* Data, int64 Size) = 0;
+    virtual bool IsLoading() const = 0;
+    virtual bool IsSaving() const = 0;
+    
+    // 플랫폼 엔디안 처리
+    virtual void SetByteSwapping(bool bInSwap) { bSwapBytes = bInSwap; }
+    
+protected:
+    bool bSwapBytes = false;
+};
+
